5b9ae72c0a49f69a67fd70e6af5f9d3f51f95e8d
Aster <84377714+h0shizaki@users.noreply.github.com>
2024-05-28 08:32:46 +0000

    init app

---
+++ app.py
@@ -0,0 +1,33 @@
+import time;
+import wiringpi;
+from wiringpi import GPIO;
+
+wiringpi.wiringPiSetup()
+pwm_pin = 2
+PWM_RANGE = 1024
+
+wiringpi.softPwmCreate(pwm_pin, 0, PWM_RANGE)
+
+
+def open_servo():
+	wiringpi.softPwmWrite(pwm_pin, 7)
+	wiringpi.delay(100)
+
+def close_servo():
+	wiringpi.softPwmWrite(pwm_pin, 19)
+	wiringpi.delay(100)
+
+
+def servo_move_to(pin, angle):
+	pwm_value = int((angle / 180 * 51 ) + 51)
+	wiringpi.softPwmWrite(pin, pwm_value)
+	wiringpi.delay(10)
+
+
+wiringpi.softPwmWrite(pwm_pin, 7)
+wiringpi.delay(100)
+time.sleep(3)
+wiringpi.softPwmWrite(pwm_pin, 19)
+wiringpi.delay(100)
+
+wiringpi.wiringPiSetup()
