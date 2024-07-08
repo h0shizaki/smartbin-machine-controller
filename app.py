0b0721a6c897f17a18f703460c39f31cf3823688
NameName <sasipha_th@cmu.ac.th>
2024-07-08 23:59:20 +0700

    move to flask

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-import servo_controller;
-import time;
-import asyncio;
-import websockets;
+import servo_controller
+import time
+from flask import Flask
 
-async def servo_control_handler(ws):
-	try:
-		servo_controller.open_servo()
-		time.sleep(1)
-		servo_controller.close_servo()
-		await ws.send("OK")
-	except:
-		print("FAILED")
+app = Flask(__name__)
 
 
-start_server = websockets.serve(servo_control_handler, "0.0.0.0", 8765)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+@app.route('/move')
+def hello_world():
+    servo_controller.open_servo()
+    time.sleep(1)
+    servo_controller.close_servo()
+    return 'OK'
+
+
+# main driver function
+if __name__ == '__main__':
+    app.run()
--- servo_controller.py
+++ servo_controller.py
@@ -1,27 +1,31 @@
 import time;
-import wiringpi;
-from wiringpi import GPIO;
+# import wiringpi;
+# from wiringpi import GPIO;
 
-wiringpi.wiringPiSetup()
+# wiringpi.wiringPiSetup()
 pwm_pin = 2
 PWM_RANGE = 1024
 
-wiringpi.softPwmCreate(pwm_pin, 0, PWM_RANGE)
+# wiringpi.softPwmCreate(pwm_pin, 0, PWM_RANGE)
 
 
 def open_servo():
-	wiringpi.softPwmWrite(pwm_pin, 7)
-	wiringpi.delay(100)
+	# wiringpi.softPwmWrite(pwm_pin, 7)
+	# wiringpi.delay(100)
+	print("Servo Open")
 
 def close_servo():
-	wiringpi.softPwmWrite(pwm_pin, 19)
-	wiringpi.delay(100)
+	# wiringpi.softPwmWrite(pwm_pin, 19)
+	# wiringpi.delay(100)
+	print("Servo Closed")
 
 
 def servo_move_to(pin, angle):
-	pwm_value = int((angle / 180 * 51 ) + 51)
-	wiringpi.softPwmWrite(pin, pwm_value)
-	wiringpi.delay(10)
+	# pwm_value = int((angle / 180 * 51 ) + 51)
+	# wiringpi.softPwmWrite(pin, pwm_value)
+	# wiringpi.delay(10)
+	print("Servo Moved to" + angle)
 
 
-wiringpi.wiringPiSetup()
+
+# wiringpi.wiringPiSetup()
